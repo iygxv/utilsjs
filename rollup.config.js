8ed5dc33e1c1d25099c930e6578e4a5f6dfd60c8
zwf <871772825@qq.com>
2023-05-19 13:57:55 +0800

    feat： 🎸增加example

--- rollup.config.js
+++ rollup.config.js
@@ -2,14 +2,17 @@ export default {
   input: './index.js',
   output: [
     {
+      name: 'utils',
       file: './dist/index.cjs.js',
       format: 'cjs'
     },
     {
+      name: 'utils',
       file: './dist/index.es.js',
       format: 'es'
     },
     {
+      name: 'utils',
       file: './dist/index.global.js',
       format: 'iife'
     }
